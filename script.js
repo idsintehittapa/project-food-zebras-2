d63da9b4082246d97b60339b08262c70e9d9bde8
idsintehittapa <jblom15@gmail.com>
2020-09-30 11:30:02 +0200

    Added sorting function

--- script.js
+++ script.js
@@ -15,23 +15,50 @@ fetch(request)
     return response.json()
   })
   .then((json) => {
-    console.log(json);
-    // console.log(json.restaurants[0].restaurant.name)
 
     const restaurantInformation = (information) => {
       const restName = information.restaurant.name
-      console.log(restName);
       const restAddress = information.restaurant.location.address
       const averageCost = information.restaurant.average_cost_for_two + " " + information.restaurant.currency
+      const cost = information.restaurant.average_cost_for_two
       const averageRating = information.restaurant.user_rating.aggregate_rating
       const image = information.restaurant.featured_image
       const estabType = information.restaurant.establishment[0]
-      return {restName, restAddress, averageRating, averageCost, image, estabType}
+      return {restName, restAddress, averageRating, averageCost, image, estabType, cost}
     }
-
     const newArray = json.restaurants.map(restaurantInformation);
+    document.getElementById('priceSorting').addEventListener('change', (event) => {
+      let sortOrder = document.getElementById('priceSorting').value 
+      sortByPrice(newArray, sortOrder);
+      let restaurantHTML = "";
+      newArray.forEach((item) => {
+        restaurantHTML += `<div>`;
+        restaurantHTML += `<img class="restroImage" src="${item.image}">`;
+        restaurantHTML += `<h3>${item.restName}</h3>`;
+        restaurantHTML += `<p>${item.restAddress}</h3>`;
+        restaurantHTML += `<p>Average cost pp ${item.averageCost}</p>`;
+        restaurantHTML += `<p>${item.estabType}</p>`;
+        restaurantHTML += `<p>${item.averageRating} &#9733;</p>`;
+        restaurantHTML += `</div>`;
+      })
+      document.getElementById('restaurantsContainer').innerHTML = restaurantHTML;
+    })
+    
 
-    console.log(newArray);
+    // price sorting function
+    const sortByPrice = (newArray, sortOrder) => {
+      console.log(sortOrder)
+      if (sortOrder === "low to high") {
+       (newArray.sort(function (a, b) {
+          return a.cost - b.cost;
+        })
+       )
+      } else if (sortOrder === "high to low") {
+        newArray.sort(function (a, b) {
+          return b.cost - a.cost;
+        })
+      }
+    }
     
     let restaurantHTML = "";
     newArray.forEach((item) => {
@@ -46,8 +73,7 @@ fetch(request)
     })
     document.getElementById('restaurantsContainer').innerHTML = restaurantHTML;
   })
-    
-    
+       
 
 
     // console.log(newArray)
@@ -61,6 +87,7 @@ fetch(request)
   // })
 
   
+
 
   
   
