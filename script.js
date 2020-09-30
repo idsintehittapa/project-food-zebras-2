e4edb24f73314aba567ef26d34e2810587f3faae
idsintehittapa <jblom15@gmail.com>
2020-09-30 11:50:11 +0200

    updated code

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 // const restaurants = document.getElementsByClassName('restaurants-container');
 const apiData = `https://developers.zomato.com/api/v2.1/search?entity_id=109&entity_type=city&cuisines=143`
 const apiKey = `cbcee325a3269e2c9a64a70beb91d113`
-
 //Header
 const request = new Request(apiData, {
   headers: new Headers({
@@ -60,35 +59,17 @@ fetch(request)
       }
     }
     
-    let restaurantHTML = "";
-    newArray.forEach((item) => {
-      restaurantHTML += `<div>`;
-      restaurantHTML += `<img class="restroImage" src="${item.image}">`;
-      restaurantHTML += `<h3>${item.restName}</h3>`;
-      restaurantHTML += `<p>${item.restAddress}</h3>`;
-      restaurantHTML += `<p>Average cost pp ${item.averageCost}</p>`;
-      restaurantHTML += `<p>${item.estabType}</p>`;
-      restaurantHTML += `<p>${item.averageRating} &#9733;</p>`;
-      restaurantHTML += `</div>`;
-    })
-    document.getElementById('restaurantsContainer').innerHTML = restaurantHTML;
+  let restaurantHTML = "";
+      newArray.forEach((item) => {
+        restaurantHTML += `<div>`;
+        restaurantHTML += `<img class="restroImage" src="${item.image}">`;
+        restaurantHTML += `<h3>${item.restName}</h3>`;
+        restaurantHTML += `<p>${item.restAddress}</h3>`;
+        restaurantHTML += `<p>Average cost pp ${item.averageCost}</p>`;
+        restaurantHTML += `<p>${item.estabType}</p>`;
+        restaurantHTML += `<p>${item.averageRating} &#9733;</p>`;
+        restaurantHTML += `</div>`;
+      })
+      document.getElementById('restaurantsContainer').innerHTML = restaurantHTML;
+    
   })
-       
-
-
-    // console.log(newArray)
-    // .forEach((item, index) => {
-    //   restaurants[index].querySelector('.rest-name').innerText = item.restName
-    //   restaurants[index].querySelector('.rest-address').innerText = item.restAddress
-    //   restaurants[index].querySelector('.rest-average-cost').innerText = item.averageCost
-    //   restaurants[index].querySelector('.rest-rating').innerText = item.averageRating
-      // restaurants[index].querySelector('.rest-picture').src = item.image
-    // })
-  // })
-
-  
-
-
-  
-  
-  
